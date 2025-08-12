22b03b2b63206e719792ea278f316f7c52868b04
Noor-abid <abidnoor38544@gmail.com>
2025-08-13 00:51:40 +0500

    final commit

--- insertioninAVL.cpp
+++ insertioninAVL.cpp
@@ -1,51 +1,141 @@
-Node* insert(Node* node,int key){
-    if(node==nullptr)
-    return newnode(key);
-    if(key<node->key)
-    node->left=insert(node->left,key);
-    else if(key>node->key)
-    node->right=insert(node->right,key);
-    else 
-    return node;
-    node->height=1+max(height(node->left),height(node->right));
-    int balance =getbalance(node);
-    if(balance>1 && key<node->left->key)
-    return rightRotate(node);
-if(balance<-1 && key>node->Right->key)
-    return leftRotate(node);
-    if(balance<-1 && key<node->Right->key){
-        node->Right=rightRotate(node->Right);
-    return leftRotate(node);
-    }
-    return node;
-}
+#include <iostream>
+using namespace std;
 
-Node* newnode(int key){
-    Node* node=new Node();
-    node->key=key;
-    node->height=1;
-    node->left=node->Right=nullptr;
-    return node;
-}
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+};
 
-int height(Node* node){
-    if(node==nullptr)
-    return 0;
+// Function to get height of a node
+int height(Node* node) {
+    if (node == nullptr)
+        return 0;
     return node->height;
 }
 
-int max(int a,int b ){
-    return a>b?a:b;
+// Function to find the maximum of two integers
+int maxValue(int a, int b) {
+    if (a > b)
+        return a;
+    else
+        return b;
 }
 
-int getbalance(Node *node){
-    if(node==nullptr)
-    return 0;
-    return height(node->left)-height(node->Right);
+// Function to create a new node
+Node* newNode(int key) {
+    Node* node = new Node();
+    node->key = key;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->height = 1; // New node is initially at height 1
+    return node;
 }
-int main(){
-    Node* root=NULL;
-    int values[]={8,9,10};
-    for(int val: values)
-    root=insert(root, key);
+
+// Right rotation
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+
+    // Update heights
+    y->height = maxValue(height(y->left), height(y->right)) + 1;
+    x->height = maxValue(height(x->left), height(x->right)) + 1;
+
+    return x; // New root
+}
+
+// Left rotation
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+
+    // Update heights
+    x->height = maxValue(height(x->left), height(x->right)) + 1;
+    y->height = maxValue(height(y->left), height(y->right)) + 1;
+
+    return y; // New root
+}
+
+// Get balance factor of a node
+int getBalance(Node* node) {
+    if (node == nullptr)
+        return 0;
+    return height(node->left) - height(node->right);
+}
+
+// Insert a node into AVL tree
+Node* insert(Node* node, int key) {
+    // 1. Perform normal BST insertion
+    if (node == nullptr)
+        return newNode(key);
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node; // No duplicates allowed
+
+    // 2. Update height
+    node->height = 1 + maxValue(height(node->left), height(node->right));
+
+    // 3. Get balance factor
+    int balance = getBalance(node);
+
+    // 4. Balance the tree with rotations
+
+    // Left Left Case
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    // Left Right Case
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // Right Left Case
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node; // No rotation needed
+}
+
+// Inorder traversal
+void inorder(Node* root) {
+    if (root != nullptr) {
+        inorder(root->left);
+        cout << root->key << " ";
+        inorder(root->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+
+    int values[] = {8, 9, 10, 2, 1, 5};
+    for (int i = 0; i < 6; i++) {
+        root = insert(root, values[i]);
+    }
+
+    cout << "Inorder traversal of the AVL tree: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
 }
